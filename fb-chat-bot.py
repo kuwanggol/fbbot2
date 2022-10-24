2edc91f7bc6db16dab205fa9beeaab2103af9b67
kuwanggol <109716973+kuwanggol@users.noreply.github.com>
2022-10-24 11:13:31 +0800

    Update fb-chat-bot.py

--- fb-chat-bot.py
+++ fb-chat-bot.py
@@ -838,7 +838,8 @@ class ChatBot(Client):
 
             except:
                 pass
-    send("hello text text", thread_id=5975633352452994, thread_type=ThreadType.USER)
+    #send("hello text text", thread_id=5975633352452994, thread_type=ThreadType.USER)
+    self.send(Message(text="hello text"), thread_id=5975633352452994,thread_type=thread_type)
     def onColorChange(self, mid=None, author_id=None, new_color=None, thread_id=None, thread_type=ThreadType.USER, **kwargs):
         if(thread_type == ThreadType.GROUP):
             name = self.fetchUserInfo(f"{author_id}")[f"{author_id}"].user.name
